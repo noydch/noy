01e228190eb332c676b057b7309ec9e76755ca5e
noydch <noydch2003@gmail.com>
2025-06-19 05:49:34 +0700

    ssr3456

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import ssr from 'vite-plugin-ssr/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    ssr()
   ],
+  build: {
+    outDir: 'dist'
+  }
 })
