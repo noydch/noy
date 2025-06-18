8ae727d75336870afddb5160c3a681d4d9350eb6
noydch <noydch2003@gmail.com>
2025-06-19 02:49:49 +0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
         <meta property="og:title" content="หน้าแรก | MyApp" />
         <meta property="og:description" content="เว็บแอปสำหรับ..." />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://yourdomain.com/" />
-        <meta property="og:image" content="https://yourdomain.com/og-image.jpg" />
+        <meta property="og:url" content="https://noytest.com/" />
+        <meta property="og:image" content="https://noytest.com/og-image.jpg" />
       </Helmet>
         <Routes />
     </>
