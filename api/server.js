d03f02556f187d4f47b677b86d467cd95833ab0f
noydch <noydch2003@gmail.com>
2025-06-19 18:00:41 +0700

    add9

--- api/server.js
+++ api/server.js
@@ -1,44 +1,41 @@
-// import fs from 'node:fs/promises'
-// import path from 'node:path'
-// import express from 'express'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import express from 'express'
 
-// const isProduction = process.env.NODE_ENV === 'production'
-// const base = process.env.BASE || '/'
+const isProduction = process.env.NODE_ENV === 'production'
+const base = process.env.BASE || '/'
 
-// // Cached production assets
-// const templateHtml = isProduction
-//     ? await fs.readFile(path.resolve('dist/client/index.html'), 'utf-8')
-//     : ''
+const templateHtml = isProduction
+    ? await fs.readFile(path.resolve(process.cwd(), 'dist/client/index.html'), 'utf-8')
+    : ''
 
-// const app = express()
+const app = express()
 
-// if (isProduction) {
-//     const compression = (await import('compression')).default
-//     const sirv = (await import('sirv')).default
-//     app.use(compression())
-//     app.use(base, sirv(path.resolve('dist/client'), { extensions: [] }))
-// }
+if (isProduction) {
+    const compression = (await import('compression')).default
+    const sirv = (await import('sirv')).default
+    app.use(compression())
+    app.use(base, sirv(path.resolve(process.cwd(), 'dist/client'), { extensions: [] }))
+}
 
-// // Serve HTML
-// app.use('*', async (req, res) => {
-//     try {
-//         const url = req.originalUrl.replace(base, '')
+app.use('*', async (req, res) => {
+    try {
+        const url = req.originalUrl.replace(base, '')
 
-//         let template = templateHtml
-//         const entryServerPath = path.resolve('dist/server/entry-server.js')
-//         const { render } = await import(entryServerPath)
+        let template = templateHtml
+        const entryServerPath = path.resolve(process.cwd(), 'dist/server/entry-server.js')
+        const { render } = await import(entryServerPath)
 
-//         const rendered = await render(url)
+        const rendered = await render(url)
+        const html = template
+            .replace(`<!--app-head-->`, rendered.head ?? '')
+            .replace(`<!--app-html-->`, rendered.html ?? '')
 
-//         const html = template
-//             .replace(`<!--app-head-->`, rendered.head ?? '')
-//             .replace(`<!--app-html-->`, rendered.html ?? '')
+        res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
+    } catch (e) {
+        console.error('SSR Error:', e)
+        res.status(500).end(e.stack)
+    }
+})
 
-//         res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
-//     } catch (e) {
-//         console.error('SSR Error:', e)
-//         res.status(500).end(e.stack)
-//     }
-// })
-
-// export default app 
+export default app
