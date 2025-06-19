b476a7aaab11e865d4af8d7423deffb5137fb918
noydch <noydch2003@gmail.com>
2025-06-19 18:14:15 +0700

    add11

--- api/server.js
+++ api/server.js
@@ -1,41 +1,71 @@
 import fs from 'node:fs/promises'
-import path from 'node:path'
 import express from 'express'
 
+// Constants
 const isProduction = process.env.NODE_ENV === 'production'
+const port = process.env.PORT || 5173
 const base = process.env.BASE || '/'
 
+// Cached production assets
 const templateHtml = isProduction
-    ? await fs.readFile(path.resolve(process.cwd(), 'dist/client/index.html'), 'utf-8')
+    ? await fs.readFile('./dist/client/index.html', 'utf-8')
     : ''
 
+// Create http server
 const app = express()
 
-if (isProduction) {
+// Add Vite or respective production middlewares
+/** @type {import('vite').ViteDevServer | undefined} */
+let vite
+if (!isProduction) {
+    const { createServer } = await import('vite')
+    vite = await createServer({
+        server: { middlewareMode: true },
+        appType: 'custom',
+        base,
+    })
+    app.use(vite.middlewares)
+} else {
     const compression = (await import('compression')).default
     const sirv = (await import('sirv')).default
     app.use(compression())
-    app.use(base, sirv(path.resolve(process.cwd(), 'dist/client'), { extensions: [] }))
+    app.use(base, sirv('./dist/client', { extensions: [] }))
 }
 
-app.use('*', async (req, res) => {
+// Serve HTML
+app.use('*all', async (req, res) => {
     try {
         const url = req.originalUrl.replace(base, '')
 
-        let template = templateHtml
-        const entryServerPath = path.resolve(process.cwd(), 'dist/server/entry-server.js')
-        const { render } = await import(entryServerPath)
+        /** @type {string} */
+        let template
+        /** @type {import('./src/entry-server.ts').render} */
+        let render
+        if (!isProduction) {
+            // Always read fresh template in development
+            template = await fs.readFile('./index.html', 'utf-8')
+            template = await vite.transformIndexHtml(url, template)
+            render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render
+        } else {
+            template = templateHtml
+            render = (await import('./dist/server/entry-server.js')).render
+        }
 
         const rendered = await render(url)
+
         const html = template
             .replace(`<!--app-head-->`, rendered.head ?? '')
             .replace(`<!--app-html-->`, rendered.html ?? '')
 
         res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
     } catch (e) {
-        console.error('SSR Error:', e)
+        vite?.ssrFixStacktrace(e)
+        console.log(e.stack)
         res.status(500).end(e.stack)
     }
 })
 
-export default app
+// Start http server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`)
+})
